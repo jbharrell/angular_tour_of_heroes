c55367072b3e92a3366a9a259c760480a9d8aef1
Jason Harrell <icubedjbh@gmail.com>
2018-03-04 17:44:51 -0500

    Part 1 complete

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,28 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  ''',
+  directives: const [formDirectives],
 )
 class AppComponent {
   var name = 'Angular';
+  final title = 'Tour of Heros';
+  Hero hero = new Hero(1, 'Windstorm');
+}
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
 }
