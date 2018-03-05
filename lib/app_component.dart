29cbaf0087c812c7f40f5d60bb0967e6d6caa217
Jason Harrell <icubedjbh@gmail.com>
2018-03-04 23:47:07 -0500

    Part 4 complete; left slow getter in place; adding loading text

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,20 +1,31 @@
+import 'dart:async';
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
+
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
 import 'src/hero_detail_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
   directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+  providers: const [HeroService],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   var name = 'Angular';
   final title = 'Tour of Heros';
   Hero selectedHero;
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
+
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroesSlowly();
+  }
+
+  void ngOnInit() => getHeroes();
 
   void onSelect(Hero hero) => selectedHero = hero;
 }
